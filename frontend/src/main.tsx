058a8f0384e85aef0d6bff542085e1a57aca9ccd
Straputicari Luca <lucastrap@live.fr>
2024-12-16 12:12:18 +0100

    Début ajout plugins dev quotes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,9 +6,12 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import App from "./App.tsx";
+import overviewContent from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
+  
   <StrictMode>
     <App />
+    <overviewContent/>
   </StrictMode>
 );
